7f01e26a1826f25fa2c490c40d0d7631cb348cb6
IvanShvedov <shvedov540@gmail.com>
2021-02-24 23:40:53 +0300

    Init

---
+++ anime_site/mainapp/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+from .choices import *
+
+
+class Anime(models.Model):
+
+    title = models.CharField(max_length=100, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    description = models.TextField(verbose_name='Описание')
+    image_url = models.URLField(verbose_name='Ссылка на картинку')
+    genres = models.ManyToManyField('Genre', verbose_name='Жанры', on_delete=models.CASCADE)
+    year = models.DateField(verbose_name='Год')
+    author = models.ForeignKey('Author', verbose_name='Автор', on_delete=models.CASCADE)
+    season = models.CharField(max_length=10, choices=SEASONS)
+    age_raiting = models.CharField(max_length=30, choices=RATING)
+    episodes = models.ManyToManyField('Episode', verbose_name='Эпизоды', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Episode(models.Model):
+    pass
+
+
+class Author(models.Model):
+    pass
+
+
+class Genre(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name='Наименование жанра')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
