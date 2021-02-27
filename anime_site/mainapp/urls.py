08925adf2242696c801c1eb08e47450e08dc9957
IvanShvedov <shvedov540@gmail.com>
2021-02-28 01:06:55 +0300

    Main page edited

--- anime_site/mainapp/urls.py
+++ anime_site/mainapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.MainView.as_view(), name='main'),
     path('anime/<str:slug>/', views.AnimeView.as_view(), name='anime page'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
